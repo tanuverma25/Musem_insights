569f84fef484d84b462136977320181f47b07858
Tanu Verma <146980832+tanuverma25@users.noreply.github.com>
2024-10-05 17:35:09 +0530

    connection.py

--- connection.py
+++ connection.py
@@ -2,12 +2,12 @@
 import pandas as pd 
 from sqlalchemy import create_engine
 
-conn_string = 'mysql+pymysql://root:your_password@localhost/painting'
+conn_string = 'mysql+pymysql://your_username:your_password@your_host/your_database'
 db = create_engine(conn_string)
 conn = db.connect()
 
 files = ['artist','canvas_size','museum','museum_hours','product_size','subject','work']
 
 for file in files:
-    df = pd.read_csv(rf'\\PC-PC\Users\PC\paintings_data\Datasets\{file}.csv')
+    df = pd.read_csv(rf'\\path\to\your\datasets\{file}.csv')
     df.to_sql(file, con=conn, if_exists='replace', index=False)
